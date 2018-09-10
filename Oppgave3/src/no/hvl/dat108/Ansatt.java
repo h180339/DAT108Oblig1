f2df1151317cd6137d501a40c26de1db0177696a
h180339 <example@example.com>
2018-09-10 08:46:57 +0200

    Startet oppgave 3

---
+++ Oppgave3/src/no/hvl/dat108/Ansatt.java
@@ -0,0 +1,10 @@
+package no.hvl.dat108;
+
+public class Ansatt {
+
+	private String fornavn;
+	private String etternavn;
+	private String kjonn;
+	private String stilling;
+	private int aarslonn;
+}
